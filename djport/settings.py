84acb231b9e26500f4933abdc198298b62181bea
skb399 <stefanboakye@gmail.com>
2026-08-02 19:31:26 +0100

    Configure Cloudinary image storage

--- djport/settings.py
+++ djport/settings.py
@@ -54,11 +54,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
-    "cloudinary",
+    "cloudinary_storage",
+    'django.contrib.sites',    
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    "cloudinary",
     "events",
     
 ]
